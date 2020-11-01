a99af76fc65f3170928d5312d685c3cf098f7625
Basti Buck <mail@bastibuck.de>
2020-11-01 01:03:38 +0100

    Add storybook (#3)
    * add storybook and basic Card
    
    * update TODO
    
    * add Card to Home

---
+++ stories/Card.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import Card, { ICardProps } from "../components/card/Card";
+
+export default {
+  title: "Molecules/Card",
+  component: Card,
+} as Meta;
+
+const Template: Story<ICardProps> = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "Booom!",
+  description: "Here's some card event description",
+  imageUrl:
+    "https://images.unsplash.com/photo-1530433331547-e064ffe8eef1?&auto=format&fit=crop&w=300",
+};
