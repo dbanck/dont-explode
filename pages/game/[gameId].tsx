28ed6ad14d38b00cf1d4c75e2119751bce7acfea
Basti Buck <mail@bastibuck.de>
2020-12-19 20:50:49 +0100

    add select overlays

--- pages/game/[gameId].tsx
+++ pages/game/[gameId].tsx
@@ -26,6 +26,9 @@ import Main from "../../components/layout/Main";
 import Wrapper from "../../components/layout/Wrapper";
 import Button from "../../components/common/Button";
 import Modal from "../../components/modal/Modal";
+import { buildCard } from "../../components/card/factory";
+import SelectCardOverlay from "../../components/overlays/SelectCardOverlay";
+import SelectPlayerOverlay from "../../components/overlays/SelectPlayerOverlay";
 
 interface IGameProps {
   user: User;
@@ -199,75 +202,6 @@ const GamePage: React.FC<IGameProps> = ({ user, games }) => {
     );
   }
 
-  const buildDummyCard = (type: Card["type"]) => {
-    switch (type) {
-      case CardType.Bomb:
-        return (
-          <CardComponent
-            title="BOOOOM!"
-            description="Show this card immediatly."
-            imageUrl="https://images.unsplash.com/photo-1530433331547-e064ffe8eef1?&auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.Defuse:
-        return (
-          <CardComponent
-            title="Defuse"
-            description="Put your last drawn card back into the deck."
-            imageUrl="https://images.unsplash.com/photo-1591455484074-d6bffbec9fae?auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.Nothing:
-        return (
-          <CardComponent
-            title="Nothing"
-            description="void"
-            imageUrl="https://images.unsplash.com/photo-1503248947681-3198a7abfcc9?auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.SeeTheFuture:
-        return (
-          <CardComponent
-            title="See the future"
-            description="Privately view the top three cards of the deck."
-            imageUrl="https://images.unsplash.com/photo-1573537805874-4cedc5d389ce?auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.Shuffle:
-        return (
-          <CardComponent
-            title="Shuffle"
-            description="Shuffle the draw pile."
-            imageUrl="https://images.unsplash.com/photo-1590232670695-17fe0b35f454?auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.Skip:
-        return (
-          <CardComponent
-            title="Skip"
-            description="End your turn without drawing a card."
-            imageUrl="https://images.unsplash.com/photo-1518365658347-c4906efc5636?auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.Attack:
-        return (
-          <CardComponent
-            title="Attack"
-            description="End your turn without drawing a card. Force the next player to take two turns."
-            imageUrl="https://images.unsplash.com/photo-1514050566906-8d077bae7046?auto=format&fit=crop&w=300"
-          />
-        );
-      case CardType.Favor:
-        return (
-          <CardComponent
-            title="Favor"
-            description="One player must give you a card of their choice."
-            imageUrl="https://images.unsplash.com/photo-1479271074763-7f7f1b06d771?auto=format&fit=crop&w=300"
-          />
-        );
-    }
-  };
-
   const otherPlayers = currentGame.players.filter(
     (player) => player !== user.id
   );
@@ -357,7 +291,7 @@ const GamePage: React.FC<IGameProps> = ({ user, games }) => {
                       }deg)`,
                     }}
                   >
-                    {buildDummyCard(card.type)}
+                    {buildCard(card.type)}
                   </div>
                 ))}
               </div>
@@ -391,7 +325,7 @@ const GamePage: React.FC<IGameProps> = ({ user, games }) => {
                   onMouseEnter={() => handleHover(card.id)}
                   onMouseLeave={() => handleUnhover(card.id)}
                 >
-                  {buildDummyCard(card.type)}
+                  {buildCard(card.type)}
                 </span>
               ))}
             </CardWrapper>
@@ -403,31 +337,22 @@ const GamePage: React.FC<IGameProps> = ({ user, games }) => {
             onRequestClose={closeCardOverlay}
           >
             {cardOverlay.map((card) => (
-              <div key={card.id}>{buildDummyCard(card.type)}</div>
+              <div key={card.id}>{buildCard(card.type)}</div>
             ))}
           </Modal>
 
-          <Modal
-            title="Select a player"
-            isOpen={typeof selectPlayerCallback === "function"}
-          >
-            {otherPlayers.map((player) => (
-              <Button onClick={() => selectPlayerCallback(player)} key={player}>
-                {currentGame.playerInfos[player].name}
-              </Button>
-            ))}
-          </Modal>
+          <SelectPlayerOverlay
+            selectPlayerCallback={selectPlayerCallback}
+            players={otherPlayers.map((player) => ({
+              id: player,
+              name: currentGame.playerInfos[player].name,
+            }))}
+          />
 
-          <Modal
-            title="Select a card"
-            isOpen={typeof selectCardCallback === "function"}
-          >
-            {hand.map((card) => (
-              <div key={card.id} onClick={() => selectCardCallback(card.id)}>
-                {buildDummyCard(card.type)}
-              </div>
-            ))}
-          </Modal>
+          <SelectCardOverlay
+            selectCardCallback={selectCardCallback}
+            cards={hand}
+          />
 
           {loseScreen && (
             <div
